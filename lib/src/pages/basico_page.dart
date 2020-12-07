bdc4abc7ae463b275c03c6e0e77fee784ee6ad88
oscar salazar <salazaroscar9705@gmail.com>
2020-12-07 08:50:27 -0500

    iniciando proyecto

---
+++ lib/src/pages/basico_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class BasicoPage extends StatelessWidget {
+  final estiloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+  final estiloSubTitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            _crearImagen(),
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearTitulo() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Lago con un puente', style: estiloTitulo),
+                  SizedBox(height: 7.0),
+                  Text('el lago es en narnia', style: estiloSubTitulo),
+                ],
+              ),
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.red,
+              size: 30.0,
+            ),
+            Text('41', style: TextStyle(fontSize: 20.0)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearImagen() {
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage(
+            'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+        height: 200.0,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _crearAcciones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _accion(Icons.call, 'CALL'),
+        _accion(Icons.near_me, 'ROUTE'),
+        _accion(Icons.share, 'SHARE'),
+      ],
+    );
+  }
+
+  Widget _accion(IconData icon, String texto) {
+    return Column(
+      children: <Widget>[
+        Icon(
+          icon,
+          color: Colors.blue,
+          size: 40.0,
+        ),
+        SizedBox(height: 5.0),
+        Text(texto, style: TextStyle(fontSize: 15.0, color: Colors.blue))
+      ],
+    );
+  }
+
+  Widget _crearTexto() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+        child: Text(
+          'asdasldkasjdklasj  dsajlasdjalsjdlasjdsa asdlasjdlkasjdlksajdjasslkas    asd asdddaskjdla djkalsjdlaskjdlsajdlkjasslkdajslkdjaslkjdalkjsdlakjsddja dasdsa ldaksjldasjdsaljasd',
+          textAlign: TextAlign.justify,
+        ),
+      ),
+    );
+  }
+}
